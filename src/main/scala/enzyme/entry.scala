4774f9bae6aef1fca0eccb710d94d31aa7254e1d
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-10-05 14:39:56 +0200

    add predicates for Enzyme classes from id

--- src/main/scala/enzyme/entry.scala
+++ src/main/scala/enzyme/entry.scala
@@ -49,3 +49,37 @@ case object Entry {
   def isValid(entry: Entry): Boolean =
     !( entry.description.startsWith("Deleted entry") || entry.description.startsWith("Transferred entry") )
 }
+
+case object EnzymeClasses {
+
+  def idFragments(id: String): (String,String,String,String) = {
+
+    val fragments = id.split('.').take(4)
+    
+    (fragments(0), fragments(1), fragments(2), fragments(3))
+  }
+
+  def isEnzyme(id: String) =
+    idFragments(id) match {
+      case (_,_,_,b) if(b != "-") => true
+      case _                      => false
+    }
+
+  def isSubSubClass(id: String) =
+    idFragments(id) match {
+      case (_,_,b,"-") if(b != "-") => true
+      case _                        => false
+    }
+
+  def isSubClass(id: String) =
+    idFragments(id) match {
+      case (_,b,"-",_) if(b != "-") => true
+      case _                        => false
+    }
+
+  def isClass(id: String) =
+    idFragments(id) match {
+      case (_,"-",_,_)  => true
+      case _            => false
+    }
+}
