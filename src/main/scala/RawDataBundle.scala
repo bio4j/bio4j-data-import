e90cd113c3699defbd794acad60e8bf067814def
Alexey Alekhin <laughedelic@gmail.com>
2016-10-09 14:20:13 +0200

    Added abstract bundle for copying files to S3

--- src/main/scala/RawDataBundle.scala
+++ src/main/scala/RawDataBundle.scala
@@ -1,7 +1,9 @@
 package com.bio4j.data
 
 import ohnosequences.statika._
+import com.amazonaws.auth._
 import ohnosequences.awstools._, s3._
+import com.amazonaws.services.s3.transfer._
 import java.net.URL
 import sys.process._
 import better.files._
@@ -31,6 +33,42 @@ abstract class GetRawData(
       // TODO: some retry logic?
     } ->-
     say(s"Files are downloaded to ${baseDirectory}")
+  }
+
+}
+
+
+abstract class CopyToS3(
+  val file: File,
+  val s3folder: S3Folder
+) extends AnyBundle {
+
+  lazy val s3client = S3.create(new InstanceProfileCredentialsProvider())
+
+  lazy val transferManager = new TransferManager(s3client.s3)
+
+  def instructions: AnyInstructions = {
+
+    LazyTry {
+      if (file.isDirectory) {
+        val target = s3folder / file.name
+
+        transferManager.upload(
+          target.bucket, target.key,
+          file.toJava
+        ).waitForCompletion
+      } else {
+
+        transferManager.uploadDirectory(
+          s3folder.bucket, s3folder.key,
+          file.toJava,
+          true // recursively
+        ).waitForCompletion
+      }
+    } -&- LazyTry {
+      transferManager.shutdownNow()
+    } ->-
+    say(s"Files are uploaded to ${s3folder.url}")
   }
 
 }
