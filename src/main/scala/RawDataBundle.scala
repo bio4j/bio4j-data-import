fccf675374bd7ddd5a23f5dfa2d2ae2bbe07814b
Alexey Alekhin <laughedelic@gmail.com>
2016-10-09 16:12:34 +0200

    Added an abstract bundle to download mirrored data

--- src/main/scala/RawDataBundle.scala
+++ src/main/scala/RawDataBundle.scala
@@ -39,36 +39,52 @@ abstract class GetRawData(
 
 
 abstract class CopyToS3(
-  val file: File,
+  val files: Seq[File],
   val s3folder: S3Folder
 ) extends AnyBundle {
 
   lazy val s3client = S3.create(new InstanceProfileCredentialsProvider())
-
   lazy val transferManager = new TransferManager(s3client.s3)
 
   def instructions: AnyInstructions = {
 
     LazyTry {
-      if (file.isDirectory) {
+      files.foreach { file =>
+
         val target = s3folder / file.name
 
         transferManager.upload(
           target.bucket, target.key,
           file.toJava
         ).waitForCompletion
-      } else {
-
-        transferManager.uploadDirectory(
-          s3folder.bucket, s3folder.key,
-          file.toJava,
-          true // recursively
-        ).waitForCompletion
       }
-    } -&- LazyTry {
+
       transferManager.shutdownNow()
     } ->-
     say(s"Files are uploaded to ${s3folder.url}")
+  }
+
+}
+
+
+abstract class GetS3Copy(
+  val s3copy: CopyToS3,
+  val baseDirectory: File
+) extends AnyBundle {
+
+  lazy val s3client = S3.create(new InstanceProfileCredentialsProvider())
+  lazy val transferManager = new TransferManager(s3client.s3)
+
+  def instructions: AnyInstructions = {
+    LazyTry {
+      transferManager.downloadDirectory(
+        s3copy.s3folder.bucket, s3copy.s3folder.key,
+        baseDirectory.toJava
+      ).waitForCompletion
+
+      transferManager.shutdownNow()
+    } ->-
+    say(s"Files are downloaded to ${baseDirectory}")
   }
 
 }
