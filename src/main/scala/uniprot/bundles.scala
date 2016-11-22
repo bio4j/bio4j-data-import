d4ef81a06e46634caa9027a32942e58114af0657
Alexey Alekhin <laughedelic@gmail.com>
2016-11-22 17:32:59 +0100

    Added UniProt

--- src/main/scala/uniprot/bundles.scala
+++ src/main/scala/uniprot/bundles.scala
@@ -1,4 +1,4 @@
-package com.bio4j.data.uniref
+package com.bio4j.data.uniprot
 
 import com.bio4j.data.bundles._
 import java.net.URL
@@ -10,19 +10,17 @@ case object bundles {
   val release = "current_release"
 
   case object fileNames {
-    val uniref50  = "uniref50.xml"   //  8.5GB gz
-    val uniref90  = "uniref90.xml"   // 15.4GB gz
-    val uniref100 = "uniref100.xml"  // 27.7GB gz
+    val sprot  = "uniprot_sprot.dat"   // 517MB gz
+    val trembl  = "uniprot_trembl.dat" // 38.9GB gz
   }
 
   // TODO: probably it's better to make 3 separate data and import bundles
   case object rawData extends GetRawData(
     urls = Seq(
-      fileNames.uniref50,
-      fileNames.uniref90,
-      fileNames.uniref100
+      fileNames.sprot,
+      fileNames.trembl
     ).map { suffix =>
-      new URL("ftp", "ftp.ebi.ac.uk", s"/pub/databases/uniprot/current_release/uniref/${suffix}/${suffix}.gz")
+      new URL("ftp", "ftp.ebi.ac.uk", s"/pub/databases/uniprot/current_release/knowledgebase/complete/${suffix}.gz")
     },
     baseDirectory = file"/media/ephemeral0/data/enzyme/",
     gunzip = true
