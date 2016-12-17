6e047ca85ab1d742b162c80087d0f92efe0410fa
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-12-17 12:41:21 +0100

    clean NCBI taxonomy code

--- src/main/scala/ncbiTaxonomy/ncbiTaxonomy.scala
+++ src/main/scala/ncbiTaxonomy/ncbiTaxonomy.scala
@@ -6,53 +6,39 @@ import com.bio4j.model._
 import com.bio4j.angulillos._
 import scala.compat.java8.OptionConverters._
 
-case class Process[V,E](val graph: NCBITaxonomyGraph[V,E]) {
+case class ImportNCBITaxonomy[V,E](val graph: NCBITaxonomyGraph[V,E]) {
 
+  // convenience aliases
   type G = NCBITaxonomyGraph[V,E]
+  def g: G = graph
 
-  val nodes = GraphProcess.generically[V,E] (graph,
-    (node: AnyNode, g: G) => {
+  def taxon(node: AnyNode, g: G): G#Taxon = {
 
-      val vertex: G#Taxon = g.taxon.addVertex
+    val vertex: G#Taxon =
+      g.taxon.addVertex
         .set(g.taxon.id, node.ID)
 
-      NCBITaxonomyGraph.TaxonomicRanks.fromString(node.rank).asScala.foreach {
-        rank => vertex.set(g.taxon.rank, rank)
+    NCBITaxonomyGraph.TaxonomicRanks.fromString(node.rank)
+      .asScala.foreach {
+        vertex.set(g.taxon.rank, _)
       }
 
-      (graph, vertex)
-    }
-  )
+    vertex
+  }
 
-  val parents = GraphProcess.generically[V,E] (graph,
-    (node: AnyNode, g: G) => {
+  def parent(node: AnyNode): Option[G#Parent] =
+    // NOTE: root has same ID for its parent, but it shouldn't have a cyclic edge
+    if (node.ID == node.parentID) None else {
 
-      val edge: Option[G#Parent] =
-        // NOTE: root has same ID for its parent, but it shouldn't have a cyclic edge
-        if (node.ID == node.parentID) None
-        else {
-          val srcOpt = g.taxon.id.index.find(node.ID).asScala
-          val tgtOpt = g.taxon.id.index.find(node.parentID).asScala
+      val srcOpt = g.taxon.id.index.find(node.ID).asScala
+      val tgtOpt = g.taxon.id.index.find(node.parentID).asScala
 
-          (srcOpt zip tgtOpt).headOption.map { case (s, t) =>
-            g.parent.addEdge(s, t)
-          }
-        }
-
-      (graph, edge)
-    }
-  )
-
-  val names = GraphProcess.generically[V,E] (graph,
-    (taxName: AnyNodeName, g: G) => {
-
-      val taxon: Option[G#Taxon] = g.taxon.id.index.find(taxName.nodeID).asScala
-
-      val vertex: Option[G#Taxon] = taxon.map {
-        _.set(g.taxon.name, taxName.name)
+      (srcOpt zip tgtOpt).headOption.map { case (s, t) =>
+        g.parent.addEdge(s, t)
       }
-
-      (graph, vertex)
     }
-  )
+
+  def name(taxName: AnyNodeName): Option[G#Taxon] =
+    g.taxon.id.index.find(taxName.nodeID).asScala
+      .map { _.set(g.taxon.name, taxName.name) }
 }
--- src/main/scala/ncbiTaxonomy/scientificName.scala
+++
@@ -1,8 +0,0 @@
-// package com.bio4j.data.ncbiTaxonomy
-//
-// // Only rows with scientific names:
-// case class ScientificName(val scientificLine: String) extends AnyVal {
-//
-//   def taxID: String = scientificLine.dmpColumns(0)
-//   def scientificName: String = scientificLine.dmpColumns(1)
-// }
