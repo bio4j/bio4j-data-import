abf2e7368636618998eb01fc6760d30513069a38
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-12-17 13:42:26 +0100

    clean UniRef code

--- src/main/scala/uniref/uniref.scala
+++ src/main/scala/uniref/uniref.scala
@@ -1,120 +1,116 @@
-// package com.bio4j.data.uniref
-//
-// import com.bio4j.data._
-// import com.bio4j.model._
-// import com.bio4j.angulillos._
-// import scala.xml._
-// import scala.compat.java8.OptionConverters._
-//
-// case class ImportProcess[V,E](val graph: UniRefGraph[V,E]) {
-//
-//   type G = UniRefGraph[V,E]
-//
-//   val uniref50 =
-//     GraphProcess.generically[V,E] (
-//       graph,
-//       (entry: Entry, g: G) =>
-//         (
-//           graph,
-//           {
-//             val cluster =
-//               g.uniRef50Cluster.addVertex
-//                 .set(g.uniRef50Cluster.id, entry.ID)
-//
-//             val representative =
-//               proteinByAccession(entry.representativeID)
-//
-//             val seed =
-//               proteinByAccession(entry.seedID)
-//
-//             val members =
-//               (representative +: entry.nonRepresentativeMemberIDs.map(proteinByAccession)).flatten
-//
-//             representative.foreach {
-//               rep => g.uniRef50Representative.addEdge(cluster, rep)
-//             }
-//
-//             seed.foreach {
-//               sd => g.uniRef50Seed.addEdge(cluster, sd)
-//             }
-//
-//             members.foreach {
-//               member => g.uniRef50Member.addEdge(cluster, member)
-//             }
-//           }
-//         )
-//     )
-//
-//   val uniref90 =
-//     GraphProcess.generically[V,E] (
-//       graph,
-//       (entry: Entry, g: G) =>
-//         (
-//           graph,
-//           {
-//             val cluster =
-//               g.uniRef90Cluster.addVertex
-//                 .set(g.uniRef90Cluster.id, entry.ID)
-//
-//             val representative =
-//               proteinByAccession(entry.representativeID)
-//
-//             val seed =
-//               proteinByAccession(entry.seedID)
-//
-//             val members =
-//               (representative +: entry.nonRepresentativeMemberIDs.map(proteinByAccession)).flatten
-//
-//             representative.foreach {
-//               rep => g.uniRef90Representative.addEdge(cluster, rep)
-//             }
-//
-//             seed.foreach {
-//               sd => g.uniRef90Seed.addEdge(cluster, sd)
-//             }
-//
-//             members.foreach {
-//               member => g.uniRef90Member.addEdge(cluster, member)
-//             }
-//           }
-//         )
-//     )
-//
-//   val uniref100 =
-//     GraphProcess.generically[V,E] (
-//       graph,
-//       (entry: Entry, g: G) =>
-//         (
-//           graph,
-//           {
-//             val cluster =
-//               g.uniRef100Cluster.addVertex
-//                 .set(g.uniRef100Cluster.id, entry.ID)
-//
-//             val representative =
-//               proteinByAccession(entry.representativeID)
-//
-//             val seed =
-//               proteinByAccession(entry.seedID)
-//
-//             val members =
-//               (representative +: entry.nonRepresentativeMemberIDs.map(proteinByAccession)).flatten
-//
-//             representative.foreach {
-//               rep => g.uniRef100Representative.addEdge(cluster, rep)
-//             }
-//
-//             seed.foreach {
-//               sd => g.uniRef100Seed.addEdge(cluster, sd)
-//             }
-//
-//             members.foreach {
-//               member => g.uniRef100Member.addEdge(cluster, member)
-//             }
-//           }
-//         )
-//     )
-//
-//   private def proteinByAccession(accession: String) =
-//     graph.uniProtGraph.protein.accession.index.find(accession).asScala
-// }
+package com.bio4j.release.generic.uniref
+
+import com.bio4j.data.uniref._
+import com.bio4j.model._
+import scala.compat.java8.OptionConverters._
+
+case class ImportUniRef[V,E](val graph: UniRefGraph[V,E]) {
+
+  type G = UniRefGraph[V,E]
+  def g: G = graph
+
+  def uniref50Cluster(clusterEntry: AnyCluster): Option[G#UniRef50Cluster] = {
+    clusterEntry.uniProtRepresentative flatMap { rep =>
+      proteinByAccession(rep.id) map { p =>
+
+        val cluster =
+          g.uniRef50Cluster.addVertex
+            .set(g.uniRef50Cluster.id, clusterEntry.ID)
+
+        val repEdge =
+          g.uniRef50Representative.addEdge(cluster, p)
+
+        val members =
+          clusterEntry.uniProtNonRepresentativeMembers
+            .collect(Function.unlift(up => proteinByAccession(up.id)))
+
+        val membersEdges =
+          members map { g.uniRef50Member.addEdge(cluster, _) }
+
+        val seedEdge =
+          clusterEntry.uniProtSeed.flatMap(up => proteinByAccession(up.id)) map {
+            g.uniRef50Seed.addEdge(cluster, _)
+          }
+
+        cluster
+      }
+    }
+  }
+
+  def uniref90Cluster(clusterEntry: AnyCluster): Option[G#UniRef90Cluster] = {
+    clusterEntry.uniProtRepresentative flatMap { rep =>
+      proteinByAccession(rep.id) map { p =>
+
+        val cluster =
+          g.uniRef90Cluster.addVertex
+            .set(g.uniRef90Cluster.id, clusterEntry.ID)
+
+        val repEdge =
+          g.uniRef90Representative.addEdge(cluster, p)
+
+        val members =
+          clusterEntry.uniProtNonRepresentativeMembers
+            .collect(Function.unlift(up => proteinByAccession(up.id)))
+
+        val membersEdges =
+          members map { g.uniRef90Member.addEdge(cluster, _) }
+
+        val seedEdge =
+          clusterEntry.uniProtSeed.flatMap(up => proteinByAccession(up.id)) map {
+            g.uniRef90Seed.addEdge(cluster, _)
+          }
+
+        cluster
+      }
+    }
+  }
+
+  def uniref100Cluster(clusterEntry: AnyCluster): Option[G#UniRef100Cluster] = {
+    clusterEntry.uniProtRepresentative flatMap { rep =>
+      proteinByAccession(rep.id) map { p =>
+
+        val cluster =
+          g.uniRef100Cluster.addVertex
+            .set(g.uniRef100Cluster.id, clusterEntry.ID)
+
+        val repEdge =
+          g.uniRef100Representative.addEdge(cluster, p)
+
+        val members =
+          clusterEntry.uniProtNonRepresentativeMembers
+            .collect(Function.unlift(up => proteinByAccession(up.id)))
+
+        val membersEdges =
+          members map { g.uniRef100Member.addEdge(cluster, _) }
+
+        val seedEdge =
+          clusterEntry.uniProtSeed.flatMap(up => proteinByAccession(up.id)) map {
+            g.uniRef100Seed.addEdge(cluster, _)
+          }
+
+        cluster
+      }
+    }
+  }
+
+  private def proteinByAccession(accession: String) =
+    graph.uniProtGraph.protein.accession.index.find(accession).asScala
+
+  implicit class ClusterOps(val cluster: AnyCluster) {
+
+    def uniProtRepresentative: Option[UniProtProtein] =
+      uniProtProteinMember( cluster.representative )
+
+    def uniProtSeed: Option[UniProtProtein] =
+      uniProtProteinMember( cluster.seed )
+
+    def uniProtNonRepresentativeMembers: Seq[UniProtProtein] =
+      cluster.nonRepresentativeMembers collect Function.unlift(uniProtProteinMember)
+
+    private def uniProtProteinMember(cm: ClusterMember): Option[UniProtProtein] =
+      cm match {
+        case p: UniProtProtein => Some(p)
+        case _ => None
+      }
+  }
+}
